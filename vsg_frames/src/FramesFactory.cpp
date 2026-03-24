52a0e2a8e6f586969d33f61c1cc280c033da147e
Malte Langosz <malte.langosz@dfki.de>
2026-03-24 11:11:31 +0100

    Fix configuration of share dir in vsg_frames

--- vsg_frames/src/FramesFactory.cpp
+++ vsg_frames/src/FramesFactory.cpp
@@ -1,11 +1,8 @@
 #include "FramesFactory.hpp"
 #include "FrameP.hpp"
+#include "config.h"
 
 #include <vsgXchange/all.h>
-
-#define MACRO_STR(name) #name
-#define MACRO_STR2(s) MACRO_STR(s)
-#define SHARE_DIR_STR MACRO_STR2(SHARE_DIR)
 
 namespace vsg_frames
 {
@@ -13,8 +10,7 @@ namespace vsg_frames
     FramesFactory::FramesFactory()
     {
         // load frames resource
-        std::string s = SHARE_DIR_STR;
-        s = s.substr(1, s.size() - 2);
+        std::string s = std::string(SHARE_DIR);
         s += "/resources/frame.obj";
         fprintf(stderr, "osg_frames: load %s\n", s.c_str());
         auto options = vsg::Options::create(vsgXchange::all::create());
